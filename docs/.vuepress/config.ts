d5030fd69b6c66d04c667e66dd1ce6b40bf82186
donggua <309366517@qq.com>
2022-01-28 04:25:48 +0800

    Initial

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,59 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+export default defineUserConfig<DefaultThemeOptions>({
+  lang: 'zh-CN',
+  title: 'Donggua',
+  description: "Donggua's Blog",
+  dest: 'dist',
+  cache: '.cache',
+  temp: '.temp',
+  head: [
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['meta', { name: 'application-name', content: "Donggua's Blog" }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'Donggua' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/images/icons/apple-touch-icon.png` }],
+    ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }]
+  ],
+
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    lastUpdated: true,
+    repo: 'https://github.com/donggua-nor/Blog',
+    repoLabel: 'Github',
+    editLink: false,
+    contributors: false,
+    navbar: [
+      {
+        text: 'JavaScript',
+        link: '/js/types'
+      },
+      {
+        text: 'Vue',
+        link: '/vue/base'
+      },
+      {
+        text: 'TypeScript',
+        link: '/ts/base'
+      }
+    ],
+    sidebar: 'auto'
+  },
+
+  plugins: [
+    ['@vuepress/plugin-pwa'],
+    [
+      '@vuepress/plugin-pwa-popup',
+      {
+        locales: {
+          '/': {
+            message: 'new features are coming',
+            buttonText: 'refresh'
+          }
+        }
+      }
+    ]
+  ]
+})
---
+++ docs/README.md
@@ -0,0 +1,27 @@
+---
+home: true
+title: Home
+heroImage: /images/hero.jpg
+heroText: null
+tagline: null
+actions:
+  - text: Start
+    link: /js/types
+features:
+  - title: Coding
+    details: 凡事总须研究，才会明白。
+  - title: Blog
+    details: 好记性不如烂笔头，记录前端知识积累。
+  - title: Standard
+    details: 为人为己，亦求问题本质已解决问题。
+footer: MIT Licensed | Copyright © 2021-present Donggua
+---
+
+```ts
+class Donggua {
+  nickname: 'donggua',
+  wechat: 'donggua_nor',
+  address: 'Guangzhou, China',
+  desc: 'Enjoying coding, Family, Cat & Food'
+}
+```
