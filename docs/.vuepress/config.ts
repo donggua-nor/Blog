81ea7facc23bde95d7a38327267180fc284d599f
donggua <309366517@qq.com>
2022-01-30 22:03:29 +0800

    chore: site assets

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,12 +9,11 @@ export default defineUserConfig<DefaultThemeOptions>({
   cache: '.cache',
   temp: '.temp',
   head: [
-    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['link', { rel: 'manifest', href: '/Blog/manifest.webmanifest' }],
     ['meta', { name: 'application-name', content: "Donggua's Blog" }],
     ['meta', { name: 'apple-mobile-web-app-title', content: 'Donggua' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: `/images/icons/apple-touch-icon.png` }],
-    ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/Blog/favicon.ico' }],
     ['meta', { name: 'msapplication-TileColor', content: '#3eaf7c' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }]
   ],
