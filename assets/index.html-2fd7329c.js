d192cd979126dd6810137142eeab4e3a5ea264ac
donggua-nor <37831399+donggua-nor@users.noreply.github.com>
2023-02-25 12:43:06 +0000

    Deploying to gh-pages from @ donggua-nor/Blog@a03eb2fa666fd697dfc9e46b2ecd294fc903fbf0 🚀

---
+++ assets/index.html-2fd7329c.js
@@ -0,0 +1,7 @@
+import{_ as n,p as s,q as a,Q as e,a1 as t}from"./framework-e93b08cb.js";const c={},o=t(`<div class="language-typescript line-numbers-mode" data-ext="ts"><pre class="language-typescript"><code><span class="token keyword">class</span> <span class="token class-name">Donggua</span> <span class="token punctuation">{</span>
+  nickname<span class="token operator">:</span> <span class="token string">&#39;donggua&#39;</span><span class="token punctuation">,</span>
+  wechat<span class="token operator">:</span> <span class="token string">&#39;donggua_nor&#39;</span><span class="token punctuation">,</span>
+  address<span class="token operator">:</span> <span class="token string">&#39;Guangzhou, China&#39;</span><span class="token punctuation">,</span>
+  company<span class="token operator">:</span> <span class="token string">&#39;CVTE&#39;</span> <span class="token comment">// 2022-present</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1);function p(i,l){return s(),a("div",null,[o,e(" /* md-file-format-disable */ ")])}const d=n(c,[["render",p],["__file","index.html.vue"]]);export{d as default};
